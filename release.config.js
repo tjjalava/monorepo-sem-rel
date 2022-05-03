a497f5b782eebb2acf788872def31c752c2c98fb
Teppo Jalava <tjjalava@iki.fi>
2022-05-03 10:41:20 +0300

    chore(common): remove plugins

--- release.config.js
+++ release.config.js
@@ -1,13 +1,5 @@
-const plugins = [
-  '@semantic-release/commit-analyzer',
-  '@semantic-release/release-notes-generator',
-  '@semantic-release/npm',
-  '@semantic-release/git'
-]
-
 const branches = [{ name: 'main' }]
 
 module.exports = {
-  branches,
-  plugins
+  branches
 }
